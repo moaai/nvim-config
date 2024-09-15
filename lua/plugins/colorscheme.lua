21a739feb523240d93b65a1ac912918b004ddba4
Jakub Gradcki <j.gradcki@moaai.com>
2024-09-15 21:45:13 +0200

    Gitsings instead of fugitive

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,17 @@
 return {
-	"rebelot/kanagawa.nvim",
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme kanagawa")
-	end,
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
+	{
+		"joshdick/onedark.vim",
+		"folke/tokyonight.nvim",
+		"NLKNguyen/papercolor-theme",
+		"morhetz/gruvbox",
+		"dracula/vim",
+		"catppuccin/nvim",
+	},
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,5 +4,6 @@ return {
 		-- This plugin automatically adjusts 'shiftwidth' and 'expandtab'
 		-- heuristically based on the current file,
 		"tpope/vim-sleuth",
+		-- "tpope/vim-fugitive",
 	},
 }
