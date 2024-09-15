ebaee3ed7cd66628f039e7abb62d557d808fb704
Jakub Gradcki <j.gradcki@moaai.com>
2024-09-15 22:12:50 +0200

    Fix lua line theme name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,9 +2,9 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		config = function()
-			local custom_gruvbox = require("lualine.themes.onedark")
+			local onedark = require("lualine.themes.onedark")
 			require("lualine").setup({
-				options = { theme = custom_gruvbox },
+				options = { theme = onedark },
 			})
 		end,
 	},
