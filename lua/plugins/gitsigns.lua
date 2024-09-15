813fc0e8257f9a999dddfc4b95a2cae7151fdfe4
Jakub Gradcki <j.gradcki@moaai.com>
2024-09-15 16:06:35 +0200

    Removed fugitive
    <leader>bl for Gitsing's blame line

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,16 +1,22 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require('gitsigns').setup {
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-      }
-    end,
-  }
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local gitsigns = require("gitsigns")
+			gitsigns.setup({
+				signs = {
+					add = { text = "+" },
+					change = { text = "~" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+			})
+
+			vim.keymap.set("n", "<leader>bl", function()
+				gitsigns.blame_line({ full = true })
+			end, { desc = "Blame Line" })
+		end,
+	},
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,8 @@
 return {
-  {
-    "nvim-tree/nvim-web-devicons",
-    -- This plugin automatically adjusts 'shiftwidth' and 'expandtab' 
-    -- heuristically based on the current file,
-    "tpope/vim-sleuth",
-    "tpope/vim-fugitive",
-  }
+	{
+		"nvim-tree/nvim-web-devicons",
+		-- This plugin automatically adjusts 'shiftwidth' and 'expandtab'
+		-- heuristically based on the current file,
+		"tpope/vim-sleuth",
+	},
 }
