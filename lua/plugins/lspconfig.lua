b613e881ecc226990efc8f37fb3c62ab4139488a
Jakub Gradcki <j.gradcki@moaai.com>
2024-09-15 22:18:41 +0200

    neodev is EOL, replaced with lazydev

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,7 +5,7 @@ return {
 
 		dependencies = {
 			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "folke/neodev.nvim", opts = {} },
+			{ "folke/lazydev.nvim", opts = {} },
 			{ "antosha417/nvim-lsp-file-operations", config = true },
 		},
 		config = function()
