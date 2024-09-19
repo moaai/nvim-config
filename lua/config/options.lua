f5e1712015a0a2a56a8f4408e5f162c357b9b0c4
Jakub Gradcki <j.gradcki@moaai.com>
2024-09-19 22:43:57 +0200

    Scrolloff and highlight when yanking

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,6 +18,9 @@ opt.autoindent = true
 
 vim.o.termguicolors = true
 
+-- Minimal number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 10
+
 opt.shortmess = vim.opt.shortmess + { c = true }
 vim.api.nvim_set_option("updatetime", 300)
 
@@ -28,3 +31,12 @@ vim.api.nvim_set_option("updatetime", 300)
 vim.cmd([[
 set signcolumn=yes
 ]])
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
