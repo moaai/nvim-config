f911bcf9e93f0803a813b24aeb1dc9cf94472177
Jakub Gradcki <j.gradcki@moaai.com>
2024-12-11 22:02:22 +0100

    Terminal keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,39 @@ keymap.set("n", "<leader>5", "5gt", { desc = "Switch to Tab 5" })
 keymap.set("n", "<leader>6", "6gt", { desc = "Switch to Tab 6" })
 keymap.set("n", "<leader>7", "7gt", { desc = "Switch to Tab 7" })
 keymap.set("n", "<leader>8", "8gt", { desc = "Switch to Tab 7" })
+
+if vim.loop.os_uname().sysname == "Linux" then
+  keymap.set(
+    "n",
+    "t",
+    ":vsplit term://fish <CR> :set nonumber <CR> :set norelativenumber <CR>",
+    { desc = "Open terminal" }
+  )
+else
+end
+
+keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h", { desc = "Left" })
+keymap.set("i", "<A-h>", "<C-\\><C-N><C-w>h", { desc = "Left" })
+
+keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l", { desc = "Right" })
+keymap.set("i", "<A-l>", "<C-\\><C-N><C-w>l", { desc = "Right" })
+
+keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k", { desc = "Top" })
+keymap.set("i", "<A-k>", "<C-\\><C-N><C-w>k", { desc = "Top" })
+
+keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j", { desc = "Bottom" })
+keymap.set("i", "<A-j>", "<C-\\><C-N><C-w>j", { desc = "Bottom" })
+
+-- :tnoremap <A-h> <C-\><C-N><C-w>h
+--
+-- :tnoremap <A-j> <C-\><C-N><C-w>j
+-- :tnoremap <A-k> <C-\><C-N><C-w>k
+-- :tnoremap <A-l> <C-\><C-N><C-w>l
+-- :inoremap <A-h> <C-\><C-N><C-w>h
+-- :inoremap <A-j> <C-\><C-N><C-w>j
+-- :inoremap <A-k> <C-\><C-N><C-w>k
+-- :inoremap <A-l> <C-\><C-N><C-w>l
+-- :nnoremap <A-h> <C-w>h
+-- :nnoremap <A-j> <C-w>j
+-- :nnoremap <A-k> <C-w>k
+-- :nnoremap <A-l> <C-w>l
