cd439ba4c9b716557fcab721d2c4d2169e812290
Jakub Gradcki <j.gradcki@moaai.com>
2025-04-11 22:38:58 +0200

    Mini status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      local onedark = require("lualine.themes.onedark")
-      require("lualine").setup({
-        options = { theme = onedark },
-      })
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   config = function()
+  --     local onedark = require("lualine.themes.onedark")
+  --     require("lualine").setup({
+  --       options = { theme = onedark },
+  --     })
+  --   end,
+  -- },
 }
