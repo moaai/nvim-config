a69ea0ad5c5ed9581c750d1767e0f73b1b9136ea
Jakub Gradcki <j.gradcki@moaai.com>
2025-04-11 21:29:13 +0200

    Switch to one dark theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,21 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme tokyonight")
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		"joshdick/onedark.vim",
-		"NLKNguyen/papercolor-theme",
-		"morhetz/gruvbox",
-		"dracula/vim",
-		"catppuccin/nvim",
-	},
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "warmer",
+      })
+      vim.cmd("colorscheme onedark")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    -- "joshdick/onedark.vim",
+    "navarasu/onedark.nvim",
+    "NLKNguyen/papercolor-theme",
+    "morhetz/gruvbox",
+    "dracula/vim",
+    "catppuccin/nvim",
+  },
 }
